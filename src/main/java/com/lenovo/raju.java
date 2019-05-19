03dd46e1ba5e85415243a14d9cea5a35be14479e
raju <raju@gmail.com>
2019-05-19 13:18:08 +0000

    raju

---
+++ src/main/java/com/lenovo/raju.java
@@ -0,0 +1,13 @@
+package com.lenovo;
+
+/**
+ * Hello world!
+ *
+ */
+public class raju 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
