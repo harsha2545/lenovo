334a934f03333523eacb2585f9e63499fd982d10
raju <raju@gmail.com>
2019-05-19 13:13:46 +0000

    web

---
+++ src/main/java/com/lenovo/web.java
@@ -0,0 +1,13 @@
+package com.lenovo;
+
+/**
+ * Hello world!
+ *
+ */
+public class web 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
